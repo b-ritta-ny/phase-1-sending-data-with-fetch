83006590748e085e6dc1c0e063be9b3b111c334a
Ritta <brittanykelly33@gmail.com>
2022-05-08 02:01:45 -0400

    commit tree d0df39ccd05bdea8b44795450cb3179b71e9e27c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+let bodyData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const renderUser = data => {
+    const span = document.createElement('span');
+    span.innerHTML = data.id;
+    document.body.append(span);
+}
+
+const configurationObj = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "accept": "application/json",
+    },
+    body: JSON.stringify(bodyData),
+};
+
+function submitData() {
+    return fetch('http://localhost:3000/users', configurationObj)
+        .then((resp) => resp.json())
+        .then((data) => renderUser(data))
+        .catch((err) => {
+            const span = document.createElement('span');
+            span.innerHTML = err.message
+            document.body.append(span);
+        })
+};
